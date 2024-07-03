91734506ed25dace0d500e21096d799b927c2001
jcolago <jcolago2005@gmail.com>
2024-07-03 17:09:54 -0500

    Added snake body and food colision

--- snake.py
+++ snake.py
@@ -39,27 +39,34 @@ window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 #initialize game
 snake = Tile(5 * TILE_SIZE, 5 * TILE_SIZE)
 food = Tile(10 * TILE_SIZE, 10 * TILE_SIZE)
+snake_body = []
 velocityX = 0
 velocityY = 0
 
 def change_direction(e):
     
     global velocityX, velocityY
-    if (e.keysym == "Up"):
+    if (e.keysym == "Up" and velocityY != 1):
         velocityX = 0
         velocityY = -1
-    elif (e.keysym == "Down"):
+    elif (e.keysym == "Down" and velocityY != -1):
         velocityX = 0
         velocityY = 1
-    elif (e.keysym == "Left"):
+    elif (e.keysym == "Left" and velocityX != 1):
         velocityX = -1
         velocityY = 0
-    elif (e.keysym == "Right"):
+    elif (e.keysym == "Right" and velocityX != -1):
         velocityX = 1
         velocityY = 0
 
 def move_snake():
     global snake
+    
+    #collision with food
+    if (snake.x == food.x and snake.y == food.y):
+        snake_body.append(Tile(food.x, food.y))
+        food.x = random.randint(0, COLS - 1) * TILE_SIZE
+        food.y = random.randint(0, ROWS - 1) * TILE_SIZE
     
     snake.x += velocityX * TILE_SIZE
     snake.y += velocityY * TILE_SIZE
@@ -70,11 +77,17 @@ def draw():
     global snake
     move_snake()
     
-    #draw snake
-    canvas.create_rectangle(snake.x, snake.y, snake.x + TILE_SIZE, snake.y + TILE_SIZE, fill="lime green")
+    canvas.delete("all")
     
     #draw food
     canvas.create_rectangle(food.x, food.y, food.x + TILE_SIZE, food.y + TILE_SIZE, fill="red")
+    
+    #draw snake
+    canvas.create_rectangle(snake.x, snake.y, snake.x + TILE_SIZE, snake.y + TILE_SIZE, fill="lime green")
+    
+    for tile in snake_body:
+        canvas.create_rectangle(tile.x, tile.y, tile.x + TILE_SIZE, tile.y + TILE_SIZE, fill="lime green")
+    
     
     window.after(100, draw)
     
