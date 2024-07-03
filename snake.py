5db91a8213a6eb2da5eadbc5a276d22cdc3c6261
jcolago <jcolago2005@gmail.com>
2024-07-03 16:42:28 -0500

    Added window canvas

--- snake.py
+++ snake.py
@@ -1,9 +1,22 @@
 import tkinter
 import random
 
+# Constants
 ROWS = 25
 COLS = 25
 TILE_SIZE = 25
 
 WINDOW_WIDTH = COLS * TILE_SIZE
 WINDOW_HEIGHT = ROWS * TILE_SIZE
+
+#game window
+window = tkinter.Tk()
+window.title("Snake")
+window.resizable(False, False)
+
+#canvas
+canvas = tkinter.Canvas(window, bg="black", width=WINDOW_WIDTH, height=WINDOW_HEIGHT, borderwidth=0, highlightthickness=0)
+canvas.pack()
+window.update()
+
+window.mainloop()
